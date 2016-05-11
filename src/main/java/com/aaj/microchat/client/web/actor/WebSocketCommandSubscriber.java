fe8c75ac3c91abb49b8a4eb85a9947501a672f25
polacofede <polacofede@outlook.com>
2016-05-11 13:44:51 -0300

    n

--- src/main/java/com/aaj/microchat/client/web/actor/WebSocketCommandSubscriber.java
+++ src/main/java/com/aaj/microchat/client/web/actor/WebSocketCommandSubscriber.java
@@ -1,12 +1,14 @@
 package com.aaj.microchat.client.web.actor;
 
 import akka.actor.Props;
+import akka.http.javadsl.model.ws.TextMessage;
+import akka.stream.actor.ActorSubscriberMessage;
 import akka.stream.actor.OneByOneRequestStrategy;
 import akka.stream.actor.RequestStrategy;
 import akka.stream.actor.UntypedActorSubscriber;
 
 /**
- * Handles commands, by sending them to a corresponding actor in the actor system.
+ * Sink, handles commands, by sending them to a corresponding actor in the actor system.
  * 
  *
  */
@@ -24,8 +26,15 @@ public class WebSocketCommandSubscriber extends UntypedActorSubscriber {
 	}
 	
 	@Override
-	public void onReceive(Object obj) throws Exception {
-		System.out.println(obj);
+	public void onReceive(Object arg) throws Exception {
+		System.out.println("subs: " + this.getSender()); 
+		if(ActorSubscriberMessage.OnNext.class.equals(arg.getClass())){
+			ActorSubscriberMessage.OnNext m = (ActorSubscriberMessage.OnNext)arg;
+			
+			System.out.println(m.element());
+		}else{
+			System.out.println(arg);
+		}
 	}
 
 	@Override
--- src/main/java/com/aaj/microchat/client/web/actor/WebSocketDataPublisherActor.java
+++ src/main/java/com/aaj/microchat/client/web/actor/WebSocketDataPublisherActor.java
@@ -1,6 +1,9 @@
 package com.aaj.microchat.client.web.actor;
 
 import akka.actor.Props;
+import akka.http.javadsl.model.ws.TextMessage;
+import akka.stream.actor.ActorPublisherMessage;
+import akka.stream.actor.ActorSubscriberMessage;
 import akka.stream.actor.UntypedActorPublisher;
 
 /**
@@ -18,9 +21,17 @@ public class WebSocketDataPublisherActor extends UntypedActorPublisher<Object> {
 	}
 	
 	@Override
-	public void onReceive(Object arg0) throws Exception {
-		
-		System.out.println(arg0);
+	public void onReceive(Object arg) throws Exception {
+		System.out.println("pub: " + this.getSender());
+		if(ActorPublisherMessage.Request.class.equals(arg.getClass())){
+			ActorPublisherMessage.Request m = (ActorPublisherMessage.Request)arg;
+			while (isActive() && totalDemand() > 0) {
+				System.out.println(m);
+				onNext(m);
+			}
+		}else{
+			System.out.println(arg);
+		}
 
 	}
 
